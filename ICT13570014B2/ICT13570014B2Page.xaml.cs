d1ee947480f9d108cb85b175b4439c55940fe68a
jeeraporn Saitanu <jeerapornsaitanu@MBPkhxneeraporn.true.true>
2017-12-21 01:14:56 +0700

    ส่งงานที่ 2

---
+++ ICT13570014B2/ICT13570014B2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace ICT13570014B2
+{
+    public partial class ICT13570014B2Page : ContentPage
+    {
+        public ICT13570014B2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
